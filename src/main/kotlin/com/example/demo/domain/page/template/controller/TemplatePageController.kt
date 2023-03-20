346cabc9bf5958b6ba7413838b5bf78efa1f6f6a
Felix D.H Kang <cozyfex@gmail.com>
2023-03-20 14:59:29 +0900

    layout and template engine

--- src/main/kotlin/com/example/demo/domain/page/template/controller/TemplatePageController.kt
+++ src/main/kotlin/com/example/demo/domain/page/template/controller/TemplatePageController.kt
@@ -2,7 +2,12 @@ package com.example.demo.domain.page.template.controller
 
 import com.example.demo.global.common.controller.BaseController
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class TemplatePageController : BaseController() {
+    @GetMapping("/template")
+    fun template(): String {
+        return "template"
+    }
 }
